acc4286acaf50255815a4d8574a4c656770d1be2
BlobTheKat <blob.kat@hotmail.com>
2024-12-16 12:51:50 +0000

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,133 @@
+#![allow(non_upper_case_globals)]
+
+#[export_name="chunk"]
+pub static mut chunk: [u64; 64] = [0; 64];
+
+#[export_name="seed"]
+pub static mut seed: [u32; 8] = [0; 8];
+
+#[export_name="offsets"]
+pub static mut p16: [f32; 25] = [0.0; 25];
+
+// Quick bijective hash
+fn hash2(a: u32, b: u32) -> u32 {
+	let x = a ^ (b * 1597334673);
+	let x = x * 0x7feb352d;
+	let x = x ^ (x >> 15);
+	x * 0x846ca68b
+}
+fn hash3(a: u32, b: u32, c: u32) -> u32 {
+	let x = a ^ (b * 1597334673);
+	let x = x * 0x7feb352d;
+	let x = x ^ (x >> 15) ^ (c * 3812015801);
+	let x = x * 0xbce6059f;
+	let x = x ^ (x >> 8);
+	x * 0x846ca68b
+}
+
+fn utof(a: u32, scale: f32) -> f32 { (a as i32 as f32) * (scale / 2147483648.0) }
+
+// Cubic lerp 3x² - 2x³
+fn lerp(a: f32, b: f32, x: f32) -> f32 { a + (b-a) * (3.0-2.0*x)*x*x }
+//fn lerp(a: f32, b: f32, x: f32) -> f32 { a + (b-a) * x }
+//fn lerp(a: f32, b: f32, x: f32) -> f32 { if x >= 0.5 { b } else { a } }
+
+#[export_name="fillNoise"]
+pub unsafe fn fill_noise(x: u32, y: u32, sd: u32) {
+	let x0 = (x-96&0xFFFFFF00)+96;
+	let y0 = (y-96&0xFFFFFF00)+96;
+	let s = hash2(seed[0]^seed[1], sd);
+	let p256_00 = utof(hash3(s, x0, y0), 0.5);
+	let p256_10 = utof(hash3(s, x0+256, y0), 0.5);
+	let p256_01 = utof(hash3(s, x0, y0+256), 0.5);
+	let p256_11 = utof(hash3(s, x0+256, y0+256), 0.5);
+	let x256 = (x-x0 >> 6) as f32;
+	let y256 = (y-y0 >> 6) as f32;
+
+	let x0 = (x-32&0xFFFFFF80)+32;
+	let y0 = (y-32&0xFFFFFF80)+32;
+	let s = hash2(seed[2], sd);
+	let p128_00 = utof(hash3(s, x0, y0), 0.25);
+	let p128_10 = utof(hash3(s, x0+128, y0), 0.25);
+	let p128_01 = utof(hash3(s, x0, y0+128), 0.25);
+	let p128_11 = utof(hash3(s, x0+128, y0+128), 0.25);
+	let x128 = (x-x0 >> 6) as f32;
+	let y128 = (y-y0 >> 6) as f32;
+
+	let s = hash2(seed[2]^seed[0], sd);
+	let p64_00 = utof(hash3(s, x, y), 0.125);
+	let p64_10 = utof(hash3(s, x+64, y), 0.125);
+	let p64_01 = utof(hash3(s, x, y+64), 0.125);
+	let p64_11 = utof(hash3(s, x+64, y+64), 0.125);
+
+	let s = hash2(seed[2]^seed[1], sd);
+	let mut p32: [f32; 9] = [0.0; 9];
+	for xd in 0..3u32 { for yd in 0..3u32 {
+		p32[(xd+yd*3) as usize] = utof(hash3(s, x + (xd<<5), y + (yd<<5)), 0.0625);
+	} }
+
+	let s = hash2(seed[3], sd);
+	for xd in 0..5u32 { for yd in 0..5u32 {
+		p16[(xd+yd*5) as usize] += utof(hash3(s, x + (xd<<4), y + (yd<<4)), 0.03125);
+	} }
+
+	let s = hash2(seed[3]^seed[0], sd);
+	let mut p8: [f32; 81] = [0.0; 81];
+	for xd in 0..9u32 { for yd in 0..9u32 {
+		p8[(xd+yd*9) as usize] = utof(hash3(s, x + (xd<<3), y + (yd<<3)), 0.015625);
+	} }
+
+	let s = hash2(seed[3]^seed[1], sd);
+	let mut p4: [f32; 289] = [0.0; 289];
+	for xd in 0..17u32 { for yd in 0..17u32 {
+		p4[(xd+yd*17) as usize] = utof(hash3(s, x + (xd<<2), y + (yd<<2)), 0.0078125);
+	} }
+
+	/*let s = hash2(seed[3]^seed[1], sd);
+	let mut p2: [f32; 1089] = [0.0; 1089];
+	for xd in 0..33u32 { for yd in 0..33u32 {
+		p2[(xd+yd*33) as usize] = utof(hash3(s, x + (xd<<1), y + (yd<<1)), 0.00390625);
+	} }*/
+
+
+	//let ch_seed = hash3(seed[1], x, y);
+	let mut y = 0;
+	let mut yf = 0f32;
+	while y < 64{
+		let mut x = 0;
+		let mut xf = 0f32;
+		let mut line: u64 = 0;
+		let p256_0 = lerp(p256_00, p256_01, (y256+yf)*0.25);
+		let p256_1 = lerp(p256_10, p256_11, (y256+yf)*0.25);
+		let p128_0 = lerp(p128_00, p128_01, (y128+yf)*0.5);
+		let p128_1 = lerp(p128_10, p128_11, (y128+yf)*0.5);
+		let p64_0 = lerp(p64_00, p64_01, yf);
+		let p64_1 = lerp(p64_10, p64_11, yf);
+		while x < 64 {
+			let base = lerp(p256_0, p256_1, (x256+xf)*0.25)
+				+ lerp(p128_0, p128_1, (x128+xf)*0.5)
+				+ lerp(p64_0, p64_1, xf);
+			xf += 0.015625;
+			let i = (x>>5) + (y>>5)*3;
+			let yf = (y&31) as f32 * 0.03125;
+			let base = base + lerp(lerp(p32[i], p32[i+3], yf), lerp(p32[i+1], p32[i+4], yf), (x&31) as f32 * 0.03125);
+			let i = (x>>4) + (y>>4)*5;
+			let yf = (y&15) as f32 * 0.0625;
+			let base = base + lerp(lerp(p16[i], p16[i+5], yf), lerp(p16[i+1], p16[i+6], yf), (x&15) as f32 * 0.0625);
+			let i = (x>>3) + (y>>3)*9;
+			let yf = (y&7) as f32 * 0.125;
+			let base = base + lerp(lerp(p8[i], p8[i+9], yf), lerp(p8[i+1], p8[i+10], yf), (x&7) as f32 * 0.125);
+			let i = (x>>2) + (y>>2)*17;
+			let yf = (y&3) as f32 * 0.25;
+			let base = base + lerp(lerp(p4[i], p4[i+17], yf), lerp(p4[i+1], p4[i+18], yf), (x&3) as f32 * 0.25);
+			//let i = (x>>1) + (y>>1)*33;
+			//let yf = (y&1) as f32 * 0.5;
+			//let base = base + lerp(lerp(p2[i], p2[i+33], yf), lerp(p2[i+1], p2[i+34], yf), (x&1) as f32 * 0.5);
+
+			if base > 0.0 { line |= 1<<x; }
+			x += 1; 
+		}
+		chunk[y] = line;
+		y += 1; yf += 0.015625;
+	}
+}
---
+++ test.js
@@ -0,0 +1,35 @@
+import fs from 'node:fs'
+import { createCanvas } from "https://deno.land/x/canvas@v1.4.2/mod.ts"
+
+const {instance: {exports}, module} = await WebAssembly.instantiate(fs.readFileSync('./target/wasm32-unknown-unknown/release/outer_perlin.wasm'))
+const startX = +process.argv[2] ?? -32, startY = +process.argv[3] ?? -32, width = +process.argv[4] || 64, height = +process.argv[5] || 64
+const ch = new Int32Array(exports.memory.buffer, +exports.chunk, 512), off = new Float32Array(exports.memory.buffer, +exports.offsets, 100)
+
+const c = createCanvas(width<<6, height<<6)
+const ctx = c.getContext('2d')
+const img = new ImageData(c.width, c.height)
+img.data[3] = 255
+const data = new Int32Array(img.data.buffer)
+const BLACK = data[0], WHITE = -1
+let time = 0
+const s = performance.now()
+for(let x = startX; x < startX+width; x++) for(let y = startY; y < startY+height; y++){
+	const s = performance.now()
+	for(let i = 0, j = 0; i < 5; i++, j += 5)
+		off[j] = off[j+1] = off[j+2] = off[j+3] = off[j+4] = ((y<<2)+i) * -.1
+	exports.fillNoise(x<<6, y<<6, 1)
+	time += performance.now() - s
+	let offset = (x-startX<<6)+((startY+height-y<<12)-64)*width
+	let black = y < 0 ? 0xFFFF4400 : BLACK
+	for(let i=0; i < 64; i++){
+		let a = ch[i<<1], b = ch[i<<1|1]
+		for(let j=0;j<32;j++) data[offset+j] = a>>j&1 ? WHITE : black, data[offset+32+j] = b>>j&1 ? WHITE : black
+		offset -= width<<6
+	}
+}
+
+console.log('Built %d chunks in %dms\n  Time spent in wasm per 1000 chunks: %sms', width*height, performance.now()-s, (time/(width*height*.001)).toFixed(3))
+
+ctx.putImageData(img, 0, 0)
+fs.writeFileSync('img.png', c.toBuffer({ type: 'image/png' }))
+c.dispose()
