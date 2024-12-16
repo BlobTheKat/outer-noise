77e1ef8ede2103d6e1ad672f8448731d609c8d74
BlobTheKat <blob.kat@hotmail.com>
2024-12-16 21:37:25 +0000

    endianness

--- README.md
+++ README.md
@@ -9,3 +9,38 @@ deno -A test.js [seed] [x] [y] [w] [h]
 deno -A test.js "creashaks organzine" -32 -10 64 20
 ```
 ![Example noise output from test.js](https://i.imgur.com/SGWoVyj.png)
+
+# Interface
+
+```js
+import { setSeed, genNoise } from 'outer-noise'
+
+setSeed('random-seed')
+// Noise offsets specified at every 16 tiles, which is interpolated by genNoise(). Negative offsets make 0/black/air more likely and positive offsets make 1/white/ground more likely
+// Here we make an offset that looks like
+// -.4 -.4 -.4 -.4 -.4
+// -.2 -.2 -.2 -.2 -.2
+//  .0  .0  .0  .0  .0
+//  .2  .2  .2  .2  .2
+//  .4  .4  .4  .4  .4
+// Note that the edges of adjacent chunks will share some values, which must be the same if you want nice continuous noise
+const offsets = new Float32Array(25)
+for(let x = 0; x <= 4; x++) for(let y = 0; y <= 4; y++){
+	offsets[x + y*5] = (y - 2) * -0.2
+}
+// Chunk coordinates
+const chX = 0, chY = 0
+// local_seed can be used to generate many (2^32) unique noises without changing the main seed
+let arr = genNoise(chX, chY, /*local_seed*/ 0)
+
+// Note: arr is only valid until the next call to genNoise(), copy it if necessary!
+noiseCache.set(chX, chY, arr = arr.slice())
+
+// arr contains 512 8-bit values arranged left-to-right, bottom-to-top, little-endian
+
+// Here we assume host endianness is little
+
+function isGround(tileX, tileY){
+	return arr[tileY<<3|tileX>>3] >> (tileX&7) & 1
+}
+```
--- glue.js
+++ glue.js
@@ -1,18 +1,20 @@
 const {instance: {exports}, module} = await WebAssembly.instantiate(Uint8Array.from(atob('{{__wasm_module__}}'), c => c.charCodeAt()))
-export const seedArray = new Int32Array(exports.memory.buffer, +exports.seed, 8)
-const ch = new Int32Array(exports.memory.buffer, +exports.chunk, 128), off = new Float32Array(exports.memory.buffer, +exports.offsets, 25)
+export const seed = new Int32Array(8)
+const mem = new DataView(exports.memory.buffer)
+const sd = exports.seed>>2, off = exports.offsets>>2
+const ch = new Uint8Array(exports.memory.buffer, +exports.chunk, 128)
 
-export function getNoise(offs, x, y, localSeed = 0){
-	off.set(offs)
+export function genNoise(offs, x, y, localSeed = 0){
+	for(let i=0;i<25;i++) mem.setFloat32((off+i)<<2, offs[i], true)
 	exports.fillNoise(x, y, localSeed)
-	return ch.slice()
+	return ch
 }
 
 const enc = new TextEncoder(), {imul} = Math
 export function setSeed(str){
 	if(str instanceof ArrayBuffer) return void(sd.set(new Int32Array(sd, 0, 8)))
 	const arr = enc.encode(str+'\0')
-	seedArray.fill(0)
+	seed.fill(0)
 	let x = 0xe336beb9|0, i = 0
 	let coeff = 1597334673
 	// Quick bijective hash
@@ -21,18 +23,19 @@ export function setSeed(str){
 		x = imul(x ^ imul(y, coeff), 0x7feb352d)
 		x ^= x >> 15
 		coeff += 0x4319fa62
-		seedArray[(i>>2)&7] ^= x
+		seed[(i>>2)&7] ^= x
 	}
 	let j = i >>= 2; coeff = 1
 	do{
 		x = imul(x, coeff += 0x6eeb828a)
 		x ^= x >> 15
-		seedArray[j = j+1&7] ^= x
+		seed[j = j+1&7] ^= x
 	}while(j != i)
+	for(let i=0;i<8;i++) mem.setInt32((sd+i)<<2, seed[i], true)
 }
 
 export function getSeedHash(){
 	let x = ''
-	for(const i of seedArray) x += (i>>>0).toString(16).padStart(8, '0')
+	for(const i of seed) x += (i>>>0).toString(16).padStart(8, '0')
 	return x
 }
